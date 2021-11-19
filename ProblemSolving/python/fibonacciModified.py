8f91a22b822ab72fbea947f0e75a78f18fa7f175
Raveen A <Raveenanbarasan@gmail.com>
2021-11-19 20:35:49 +0530

    fibonacci modified completed

---
+++ ProblemSolving/python/fibonacciModified.py
@@ -0,0 +1,40 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'fibonacciModified' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER t1
+#  2. INTEGER t2
+#  3. INTEGER n
+#
+
+def fibonacciModified(t1, t2, n):
+    if n == 1:
+        return t1
+    if n == 2:
+        return t2
+    return fibonacciModified(t1, t2, n-2) + fibonacciModified(t1, t2, n-1) ** 2
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    t1 = int(first_multiple_input[0])
+
+    t2 = int(first_multiple_input[1])
+
+    n = int(first_multiple_input[2])
+
+    result = fibonacciModified(t1, t2, n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
