660d3057ce00e33b0684299830ba40b294e79ba0
Raveen A <Raveenanbarasan@gmail.com>
2021-12-06 19:16:24 +0530

    counting valley solved

---
+++ Hackerrank/ProblemSolving/java/CountingValley.java
@@ -0,0 +1,56 @@
+package Hackerrank.ProblemSolving.java;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'countingValleys' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     * 1. INTEGER steps
+     * 2. STRING path
+     */
+
+    public static int countingValleys(int steps, String path) {
+        // Write your code here
+        int v = 0;
+        int level = 0;
+        for (char c : path.toCharArray()) {
+            if (c == 'U')
+                level++;
+            else
+                level--;
+            if (level == 0 && c == 'U')
+                v++;
+        }
+        return v;
+    }
+
+}
+
+public class CountingValley {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int steps = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String path = bufferedReader.readLine();
+
+        int result = Result.countingValleys(steps, path);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
