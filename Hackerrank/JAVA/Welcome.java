04cf094651d1b96d3da50e43c644eda49b5d685c
Raveen A <Raveenanbarasan@gmail.com>
2021-12-08 10:01:58 +0530

    Welcome to java completed

--- Hackerrank/JAVA/Welcome.java
+++ Hackerrank/JAVA/Welcome.java
@@ -1,5 +1,16 @@
 package Hackerrank.JAVA;
 
+import java.io.*;
+import java.util.*;
+
 public class Welcome {
 
+    public static void main(String[] args) {
+        /*
+         * Enter your code here. Read input from STDIN. Print output to STDOUT. Your
+         * class should be named Solution.
+         */
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
 }
