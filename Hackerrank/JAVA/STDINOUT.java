4c6932f2c86e50934fef2a19a67970fc79fb79fa
Raveen A <Raveenanbarasan@gmail.com>
2021-12-08 10:06:22 +0530

    java stdin stdout 1 completed

---
+++ Hackerrank/JAVA/STDINOUT.java
@@ -0,0 +1,20 @@
+package Hackerrank.JAVA;
+
+import java.io.*;
+import java.util.*;
+
+public class STDINOUT {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a, b, c;
+        a = scanner.nextInt();
+        b = scanner.nextInt();
+        c = scanner.nextInt();
+        scanner.close();
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
