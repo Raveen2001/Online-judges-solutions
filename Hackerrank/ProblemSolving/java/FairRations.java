73b196a0b5dda57b8a3eff619395d2241082f961
Raveen A <Raveenanbarasan@gmail.com>
2021-12-10 11:14:46 +0530

    fair rations completed

---
+++ Hackerrank/ProblemSolving/java/FairRations.java
@@ -0,0 +1,62 @@
+package Hackerrank.ProblemSolving.java;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'fairRations' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts INTEGER_ARRAY B as parameter.
+     */
+
+    public static String fairRations(List<Integer> B) {
+        int count = 0;
+        int i;
+        for (i = 0; i < B.size() - 1; i++) {
+            if (B.get(i) % 2 != 0) {
+                count += 2;
+                B.set(i, B.get(i) + 1);
+                B.set(i + 1, B.get(i + 1) + 1);
+            }
+        }
+        if (B.get(i) % 2 == 0)
+            return String.valueOf(count);
+        else
+            return "NO";
+
+    }
+
+}
+
+public class FairRations {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> B = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        String result = Result.fairRations(B);
+
+        bufferedWriter.write(result);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
