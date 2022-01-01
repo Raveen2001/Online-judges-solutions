f999857f877c0d77847c975896949a737e45e9e1
Raveen2001 <raveenanbarasan@gmail.com>
2022-01-01 19:56:42 +0530

    partition label -- leet code done

---
+++ LeetCode/PartitionLabel.java
@@ -0,0 +1,24 @@
+package LeetCode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class PartitionLabel {
+    public List<Integer> partitionLabels(String s) {
+        int[] lastOcc = new int[26];
+        int i, n = s.length();
+        for (i = 0; i < n; i++) {
+            lastOcc[s.charAt(i) - 'a'] = i;
+        }
+        List<Integer> ans = new ArrayList<>();
+        int start = 0, end = 0;
+        for (i = 0; i < n; i++) {
+            end = Math.max(end, lastOcc[s.charAt(i) - 'a']);
+            if (end == i) {
+                ans.add(end - start + 1);
+                start = i + 1;
+            }
+        }
+        return ans;
+    }
+}
---
+++ SuperPACC/PhaseI/Symmetric.java
@@ -0,0 +1,24 @@
+package SuperPACC.PhaseI;
+
+import java.util.Scanner;
+
+public class Symmetric {
+    public static void main(String[] args) {
+        int t;
+        int n;
+        String[] arr;
+        Scanner scanner = new Scanner(System.in);
+        t = scanner.nextInt();
+        for(int x = 0; x<t; x++){
+            n = scanner.nextInt();
+            arr = new String[n];
+            scanner.nextLine();
+            for(int i = 0; i<n; i++){
+                arr[i] = scanner.nextLine();
+            }
+            for(int i = 0; i<n; i++){
+                
+            }
+        }
+    }
+}
