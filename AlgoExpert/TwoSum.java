53493874a608a5ad593eea40b13036bd50d240c6
Raveen2001 <raveenanbarasan@gmail.com>
2022-02-25 18:44:13 +0530

    two sum solution 2 -- algo expert

--- AlgoExpert/TwoSum.java
+++ AlgoExpert/TwoSum.java
@@ -19,3 +19,29 @@ public class TwoSum {
   }
 }
 
+
+// solution 2
+
+
+import java.util.*;
+
+class Program {
+  public static int[] twoNumberSum(int[] array, int targetSum) {
+    Arrays.sort(array);
+		int left = 0;
+		int right = array.length - 1;
+		while(left < right){
+			int sum = array[left] + array[right];
+			if(sum == targetSum){
+				return new int[]{array[left], array[right]};
+			}else if(sum > targetSum){
+				right--;
+			}else{
+				left++;
+			}
+		}
+		
+    return new int[0];
+  }
+}
+
