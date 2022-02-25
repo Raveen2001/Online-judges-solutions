43e863edcd7e734083cc635117645f1d28afbd3d
Raveen2001 <raveenanbarasan@gmail.com>
2022-02-25 19:28:18 +0530

    tournament winner code optimised -- algo expert

--- AlgoExpert/TournamentWinner.java
+++ AlgoExpert/TournamentWinner.java
@@ -8,27 +8,25 @@ class Program {
       ArrayList<ArrayList<String>> competitions, ArrayList<Integer> results) {
     // Write your code here.
 		Map<String, Integer> pointsTable = new HashMap<>();
-		String winningTeam = "";
+		String currentBestTeam = "";
+		pointsTable.put(currentBestTeam, 0);
 		for(int i = 0; i<results.size(); i++){
-			if(results.get(i) == 0){
-				winningTeam = competitions.get(i).get(1);
-			}else{
-				winningTeam = competitions.get(i).get(0);
+			String winningTeam = "";
+			winningTeam = results.get(i) == 1 ? competitions.get(i).get(0) :competitions.get(i).get(1);
+			updateWinningTeamScore(winningTeam, pointsTable);
+			if(pointsTable.get(winningTeam) > pointsTable.get(currentBestTeam)){
+				currentBestTeam = winningTeam;
 			}
-			if(pointsTable.containsKey(winningTeam)){
+		}
+    return currentBestTeam;
+  }
+	
+	
+	public void updateWinningTeamScore(String winningTeam, Map<String, Integer> pointsTable){
+		if(pointsTable.containsKey(winningTeam)){
 				pointsTable.put(winningTeam, pointsTable.get(winningTeam) + 3);
 			}else{
 				pointsTable.put(winningTeam, 3);
 			}
-		}
-		int maxPoints = 0;
-		for(Map.Entry<String, Integer> entry: pointsTable.entrySet()){
-			System.out.println(entry.getKey() + " : " + entry.getValue());
-			if(entry.getValue() > maxPoints){
-				winningTeam = entry.getKey();
-				maxPoints = entry.getValue();
-			} 
-		}
-    return winningTeam;
-  }
+	}
 }
