0b913d6255475b3071a3cd05b9353a1b2d2db51d
Raveen2001 <raveenanbarasan@gmail.com>
2022-03-01 17:08:23 +0530

    remove duplicates in linked list -- algo expert

--- AlgoExpert/RemoveDuplicatedInLinkedList.java
+++ AlgoExpert/RemoveDuplicatedInLinkedList.java
@@ -26,3 +26,38 @@ class Program {
   }
 }
 
+
+
+
+// 2 method --> not optimal
+
+
+import java.util.*;
+
+class Program {
+  // This is an input class. Do not edit.
+  public static class LinkedList {
+    public int value;
+    public LinkedList next;
+
+    public LinkedList(int value) {
+      this.value = value;
+      this.next = null;
+    }
+  }
+
+  public LinkedList removeDuplicatesFromLinkedList(LinkedList linkedList) {
+    var list = new ArrayList<Integer>();
+		list.add(linkedList.value);
+		var head = linkedList;
+		while(linkedList.next != null){
+			if(!list.contains(linkedList.next.value)){
+				list.add(linkedList.next.value);
+				linkedList = linkedList.next;
+			}else{
+				linkedList.next = linkedList.next.next;
+			}
+		}
+    return head;
+  }
+}
