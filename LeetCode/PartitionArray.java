2da8e6db1316ae408d23113aa89ffbe03f4bb400
Raveen2001 <raveenanbarasan@gmail.com>
2022-03-01 15:54:04 +0530

    partition sum -- leet code optimised

--- LeetCode/PartitionArray.java
+++ LeetCode/PartitionArray.java
@@ -1,18 +1,29 @@
 package LeetCode;
+
+import java.util.stream.IntStream;
+
 // 1013. Partition Array Into Three Parts With Equal Sum
 class Solution {
     public boolean canThreePartsEqualSum(int[] arr) {
         int sum = IntStream.of(arr).sum();
         if(sum % 3 !=0 ) return false;
         int avg = sum/3;
-        int partSum = 0, c = 0;
-        for(int a : arr){
-            partSum += a;
+        int partSum = 0, nextStart = -1;
+        for(int i = 0; i<arr.length - 2; i++){
+            partSum += arr[i];
             if(partSum == avg){
-                c++;
-                partSum = 0;
+                nextStart = i+1;
+                break;
             }
         }
-        return c>=3;
+        if(nextStart == -1) return false;
+        partSum = 0;
+        for(int i = nextStart; i<arr.length - 1; i++){
+            partSum += arr[i];
+            if(partSum == avg){
+                return true;
+            }
+        }
+        return false;
     }
 }
