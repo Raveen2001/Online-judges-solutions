f83ab3ee202f9ef4ec874e23c1aea6bdaa41ee2f
Raveen2001 <raveenanbarasan@gmail.com>
2022-03-01 17:54:04 +0530

    product sum -- algo expert

--- AlgoExpert/ProductSum.java
+++ AlgoExpert/ProductSum.java
@@ -21,3 +21,33 @@ class Program {
     return productSum;
 	}
 }
+
+
+
+// 2 method
+
+
+import java.util.*;
+
+class Program {
+  // Tip: You can use `element instanceof ArrayList` to check whether an item
+  // is an array or an integer.
+  public static int productSum(List<Object> array) {
+    return productSum(array, 1);
+  }
+	
+	public static int productSum(List<Object> array, int depth){
+		int productSum = 0;
+		for(Object e : array){
+			if(e instanceof ArrayList){
+				@SuppressWarnings("unchecked")
+				List<Object> ls = (List<Object>)e;
+				productSum += productSum(ls, depth + 1);
+			}else{
+				productSum += (int)e;
+			}
+		}
+    return depth * productSum;
+	}
+}
+
