728707b342eb36676972f43320896e14caada58c
Raveen2001 <raveenanbarasan@gmail.com>
2022-03-01 17:26:16 +0530

    NthFib -- algo expert

--- AlgoExpert/NthFIb.java
+++ AlgoExpert/NthFIb.java
@@ -33,3 +33,20 @@ class Program {
 
 
 
+//3 method --> optimal
+
+import java.util.*;
+
+class Program {
+  public static int getNthFib(int n) {
+		int[] lastTwo = {0, 1};
+		int counter = 3;
+		while(counter <=n){
+			int nextFib = lastTwo[0] + lastTwo[1];
+			lastTwo[0] = lastTwo[1];
+			lastTwo[1] = nextFib;
+			counter ++;
+		}
+		return (n>1)?lastTwo[1]: lastTwo[0];
+	}
+}
