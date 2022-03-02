fdad4f71fc7baae135642e0f80304df85b38f350
Raveen2001 <raveenanbarasan@gmail.com>
2022-03-02 19:03:33 +0530

    pallindrome -- algo expert

---
+++ AlgoExpert/Pallindrome.java
@@ -0,0 +1,16 @@
+package AlgoExpert;
+
+import java.util.*;
+
+class Program {
+  public static boolean isPalindrome(String str) {
+		int left = 0;
+		int right = str.length() - 1;
+		while(left < right){
+			if(str.charAt(left) != str.charAt(right)) return false;
+			left ++;
+			right --;
+		}
+    return true;
+  }
+}
