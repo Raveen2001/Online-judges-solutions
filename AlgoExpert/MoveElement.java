dc61a11e6f7ac753b662a96958563d3e04092714
Raveen2001 <raveenanbarasan@gmail.com>
2022-03-02 23:27:35 +0530

    Move element -- algoexpert

--- AlgoExpert/MoveElement.java
+++ AlgoExpert/MoveElement.java
@@ -30,3 +30,27 @@ class Program {
 
 
 
+// method 2 -- more readable
+
+
+import java.util.*;
+
+class Program {
+  public static List<Integer> moveElementToEnd(List<Integer> array, int toMove) {
+		int i = 0;
+		int j = array.size() - 1;
+		while(i<j){
+			while(i<j && array.get(j) == toMove) j--;
+			if(array.get(i) == toMove) swap(array, i, j);
+			i++;
+		}
+    return array;
+  }
+	
+	public static void swap(List<Integer> array, int i, int j){
+		int temp = array.get(i);
+		array.set(i, array.get(j));
+		array.set(j, temp);
+	}
+}
+
