708051e51e0b47b97cdf285901ded42408b19240
Raveen2001 <raveenanbarasan@gmail.com>
2022-03-03 09:46:35 +0530

    mono atomic array -- algoexpert

--- AlgoExpert/MonoAtomicArray.java
+++ AlgoExpert/MonoAtomicArray.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 class Program {
   public static boolean isMonotonic(int[] array) {
-    if(array.length <=2*/) return true;
+    if(array.length <=2) return true;
 		boolean nonIncreasing = false;
 		int right = array.length - 1;
 		while(right >= 0){
