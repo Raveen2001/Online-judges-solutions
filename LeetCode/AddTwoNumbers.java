b1a8d6542e5ba97ac7c85f2c4f8c11d03e20050c
Raveen2001 <raveenanbarasan@gmail.com>
2022-03-07 16:11:02 +0530

    add two numbers -- leetcode -- optimized

--- LeetCode/AddTwoNumbers.java
+++ LeetCode/AddTwoNumbers.java
@@ -50,3 +50,36 @@ class Solution {
         return res.next;
     }
 }
+
+
+
+// method 2 -- optimal
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    int carry = 0;
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        if(l1 == null && l2 == null && carry == 0) return null;
+        
+        int a = l1 != null ? l1.val : 0;
+        int b = l2 != null ? l2.val : 0;
+        
+        int sum = a + b + carry;
+        carry = sum / 10;
+        
+        l1 = l1 != null ? l1.next : null;
+        l2 = l2 != null ? l2.next : null;
+        
+        return new ListNode(sum%10, addTwoNumbers(l1, l2));
+    }
+}
