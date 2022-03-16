f588b3397a2e1e722eae872011fba563cc9b6e3d
Raveen2001 <raveenanbarasan@gmail.com>
2022-03-16 21:42:26 +0530

    fibonacci number -- leetcode

---
+++ LeetCode/FibonacciNumber.java
@@ -0,0 +1,16 @@
+package LeetCode;
+
+class Solution {
+    public int fib(int n) {
+        if(n < 2) return n;
+        int a = 0;
+        int b = 1;
+        int c = 0;
+        for(int i = 2; i<=n; i++){
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        return c;
+    }
+}
