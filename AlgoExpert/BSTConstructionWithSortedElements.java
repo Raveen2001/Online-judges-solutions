d9a9589d148be9e0adbf977006cdc698aeef1cc2
Raveen2001 <raveenanbarasan@gmail.com>
2022-03-17 10:39:58 +0530

    Bst construction with sorted elements with min height -- algo expert --  space optimized

--- AlgoExpert/BSTConstructionWithSortedElements.java
+++ AlgoExpert/BSTConstructionWithSortedElements.java
@@ -42,3 +42,53 @@ class Program {
   }
 }
 
+
+
+
+// space optimized 
+import java.util.*;
+
+class Program {
+  public static BST minHeightBst(List<Integer> array) {
+		return minHeightBst(array, 0, array.size() - 1);
+  }
+	
+	
+	public static BST minHeightBst(List<Integer> array, int startIdx, int endIdx){
+		if(startIdx > endIdx) return null;
+  	int mid = (startIdx + endIdx) / 2;
+		int midValue = array.get(mid);
+		BST node = new BST(midValue);
+		node.left = minHeightBst(array, startIdx, mid - 1);
+		node.right = minHeightBst(array, mid + 1, endIdx);
+    return node;
+	}
+
+  static class BST {
+    public int value;
+    public BST left;
+    public BST right;
+
+    public BST(int value) {
+      this.value = value;
+      left = null;
+      right = null;
+    }
+
+    public void insert(int value) {
+      if (value < this.value) {
+        if (left == null) {
+          left = new BST(value);
+        } else {
+          left.insert(value);
+        }
+      } else {
+        if (right == null) {
+          right = new BST(value);
+        } else {
+          right.insert(value);
+        }
+      }
+    }
+  }
+}
