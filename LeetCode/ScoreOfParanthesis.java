ae4d00165cfbb85e55f8a5bb612352749f4e9d39
Raveen2001 <raveenanbarasan@gmail.com>
2022-03-17 14:55:40 +0530

    score of paranthesis -- leetcode

---
+++ LeetCode/ScoreOfParanthesis.java
@@ -0,0 +1,20 @@
+package LeetCode;
+
+import java.util.Stack;
+
+class Solution {
+    public int scoreOfParentheses(String s) {
+        int cur = 0;
+        Stack<Integer> stack = new Stack<>();
+        for(char c: s.toCharArray()){
+            if(c == '('){
+                stack.push(cur);
+                cur = 0;
+            } 
+            else{
+                cur = stack.pop() + Math.max(cur * 2, 1);
+            }
+        }
+        return cur;
+    }
+}
