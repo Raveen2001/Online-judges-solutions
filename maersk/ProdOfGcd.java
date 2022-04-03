33fa22771a54065c20775159f558b763a7b69e76
Raveen2001 <raveenanbarasan@gmail.com>
2022-04-03 19:03:36 +0530

    maersk - sum of gcd

---
+++ maersk/ProdOfGcd.java
@@ -0,0 +1,20 @@
+public class ProdOfGcd {
+    public static void main(String[] args) {
+        
+    }
+
+    static int sumOfGcd(int n){
+        int sum = 0;
+        for(int i = 1; i<= n; i++){
+            int tempProd = i * gcd(i, n);
+            sum += tempProd;
+        }
+        return sum;
+    }
+
+    static int gcd(int a, int b){
+        if(b == 0) return a;
+        return gcd(b, a%b);
+    }
+
+}
